47574e7b29bebcd17b5fb3af6b47ecb2feb70a42
Haris-Ali <haris.ali@vizteck.com>
2024-12-17 23:31:46 +0000

    Fixed tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+	content: [
+		"./src/**/*.{html,ts,css,scss}",
+		"./node_modules/primeng/**/*.{js,ts,css,scss}",
+		"./node_modules/tailwindcss-primeui/**/*.{js,ts,css,scss}",
+	],
+	theme: {
+		extend: {},
+	},
 	plugins: [require("tailwindcss-primeui")],
 };

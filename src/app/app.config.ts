7aab808531e1119e7ec8ca2c66f51fedb7c1a4fd
Haris-Ali <haris.ali@vizteck.com>
2024-12-29 22:22:25 +0000

    Changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+	provideRouter,
+	withEnabledBlockingInitialNavigation,
+	withInMemoryScrolling,
+} from "@angular/router";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { provideHttpClient, withFetch } from "@angular/common/http";
 import { providePrimeNG } from "primeng/config";
 import Aura from "@primeng/themes/aura";
 
@@ -9,8 +14,16 @@ import { routes } from "./app.routes";
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideRouter(routes),
+		provideRouter(
+			routes,
+			withInMemoryScrolling({
+				anchorScrolling: "enabled",
+				scrollPositionRestoration: "enabled",
+			}),
+			withEnabledBlockingInitialNavigation()
+		),
 		provideAnimationsAsync(),
+		provideHttpClient(withFetch()),
 		providePrimeNG({
 			theme: {
 				preset: Aura,
